5d5df090a825dd822cb4d77c0eb8472a913295a2
Jarod Lopez <159594054+jarodlopez16@users.noreply.github.com>
2024-09-10 19:23:23 -0400

    Add files via upload

---
+++ poke_fundamentals.py
@@ -0,0 +1,5 @@
+# base url for pokemon
+base_url = "https://pokeapi.co/api/v2/"
+
+# list of all typings
+TYPES = ["normal", "fire", "water", "electric", "grass", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dragon", "dark", "steel", "fairy"]
