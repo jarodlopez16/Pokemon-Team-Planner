780c67d41a297eaebe952eb3a03a110431f0b9cc
Jarod Lopez <159594054+jarodlopez16@users.noreply.github.com>
2024-09-16 18:53:41 -0400

    Update matchups.py

--- matchups.py
+++ matchups.py
@@ -5,13 +5,13 @@ def type_matchups(offe, defe): # provides effectiveness of each typing matchup
     # list of all pokemon typings
     o_type = pb.type_(offe)
     if defe in [typ.name for typ in o_type.damage_relations.no_damage_to]:
-        return 0.0
+        print ("Immune")
     elif defe in [typ.name for typ in o_type.damage_relations.half_damage_to]:
-        return 0.5
+        print("Ineffective")
     elif defe in [typ.name for typ in o_type.damage_relations.double_damage_to]:
-        return 2.0
+        print("Super Effective")
     else:
-        return 1.0
+        print("Effective")
     
 
 def poke_offensive_matchups(typings):
@@ -25,36 +25,36 @@ def poke_offensive_matchups(typings):
                 if i == 0:
                     typ = pb.type_(typings[i])
                     if other_type in [t.name for t in typ.damage_relations.no_damage_to]:
-                        effective1[other_type] = 0.0
+                        effective1[other_type] = "Immune"
                     elif other_type in [t.name for t in typ.damage_relations.half_damage_to]:
-                        effective1[other_type] = 0.5
+                        effective1[other_type] = "Ineffective"
                     elif other_type in [t.name for t in typ.damage_relations.double_damage_to]:
-                        effective1[other_type] = 2.0
+                        effective1[other_type] = "Super Effective"
                     else:
-                        effective1[other_type] = 1.0
+                        effective1[other_type] = "Effective"
                 else:
                     typ = pb.type_(typings[i])
                     if other_type in [t.name for t in typ.damage_relations.no_damage_to]:
-                        effective2[other_type] = 0.0
+                        effective2[other_type] = "Immune"
                     elif other_type in [t.name for t in typ.damage_relations.half_damage_to]:
-                        effective2[other_type] = 0.5
+                        effective2[other_type] = "Ineffective"
                     elif other_type in [t.name for t in typ.damage_relations.double_damage_to]:
-                        effective2[other_type] = 2.0
+                        effective2[other_type] = "Super effective"
                     else:
-                        effective2[other_type] = 1.0
+                        effective2[other_type] = "Ineffective"
         print(f"'{typings[0].capitalize()}' Type Offensive Matchups: {effective1}")
         print(f"'{typings[1].capitalize()}' Type Offensive Matchups: {effective2}")
     else:   # if pokemon is single-typed
         for other_type in TYPES:
                 typ = pb.type_(typings[0])
                 if other_type in [t.name for t in typ.damage_relations.no_damage_to]:
-                    effective1[other_type] = 0.0
+                    effective1[other_type] = "Immune"
                 elif other_type in [t.name for t in typ.damage_relations.half_damage_to]:
-                    effective1[other_type] = 0.5
+                    effective1[other_type] = "Ineffective"
                 elif other_type in [t.name for t in typ.damage_relations.double_damage_to]:
-                    effective1[other_type] = 2.0
+                    effective1[other_type] = "Super effective"
                 else:
-                    effective1[other_type] = 1.0
+                    effective1[other_type] = "Effective"
         print(f"'{typings[0].capitalize()}' Type Offensive Matchups: {effective1}")
 
 
@@ -69,36 +69,36 @@ def poke_defensive_matchups(typings):
                 if i == 0:
                     typ = pb.type_(typings[i])
                     if other_type in [t.name for t in typ.damage_relations.no_damage_from]:
-                        weakness1[other_type] = 0.0
+                        weakness1[other_type] = "Immune"
                     elif other_type in [t.name for t in typ.damage_relations.half_damage_from]:
-                        weakness1[other_type] = 0.5
+                        weakness1[other_type] = "Ineffective"
                     elif other_type in [t.name for t in typ.damage_relations.double_damage_from]:
-                        weakness1[other_type] = 2.0
+                        weakness1[other_type] = "Super Effective"
                     else:
-                        weakness1[other_type] = 1.0
+                        weakness1[other_type] = "Effective"
                 else:
                     typ = pb.type_(typings[i])
                     if other_type in [t.name for t in typ.damage_relations.no_damage_from]:
-                        weakness2[other_type] = 0.0
+                        weakness2[other_type] = "Immune"
                     elif other_type in [t.name for t in typ.damage_relations.half_damage_from]:
-                        weakness2[other_type] = 0.5
+                        weakness2[other_type] = "Ineffective"
                     elif other_type in [t.name for t in typ.damage_relations.double_damage_from]:
-                        weakness2[other_type] = 2.0
+                        weakness2[other_type] = "Super effective"
                     else:
-                        weakness2[other_type] = 1.0
+                        weakness2[other_type] = "Effective"
         print(f"'{typings[0].capitalize()}' Type Defensive Matchups: {weakness1}")
         print(f"'{typings[1].capitalize()}' Type Defensive Matchups: {weakness2}")
     else:   # if pokemon if single-typed
         for other_type in TYPES:
             typ = pb.type_(typings[0])
             if other_type in [t.name for t in typ.damage_relations.no_damage_from]:
-                    weakness1[other_type] = 0.0
+                    weakness1[other_type] = "Immune"
             elif other_type in [t.name for t in typ.damage_relations.half_damage_from]:
-                    weakness1[other_type] = 0.5
+                    weakness1[other_type] = "Ineffective"
             elif other_type in [t.name for t in typ.damage_relations.double_damage_from]:
-                    weakness1[other_type] = 2.0
+                    weakness1[other_type] = "Super Effective"
             else:
-                    weakness1[other_type] = 1.0
+                    weakness1[other_type] = "Effective"
         print(f"'{typings[0].capitalize()}' Type Offensive Matchups: {weakness1}")
 
 
@@ -119,15 +119,15 @@ def get_effectiveness(typings):
                     if against in [t.name for t in typ.damage_relations.double_damage_to]:
                         eff_against2.append(against)
                     eff_list2 = ", ".join(str(eff_type.capitalize()) for eff_type in eff_against2)
-        print(f"Its {typings[0].capitalize()} typing is effective against: " + eff_list1 + " types.")    
-        print(f"Its {typings[1].capitalize()} typing is effective against: " + eff_list2 + " types.")
+        print(f"Its {typings[0].capitalize()} typing is super effective against: " + eff_list1 + " types.")    
+        print(f"Its {typings[1].capitalize()} typing is super effective against: " + eff_list2 + " types.")
     else:
         for against in TYPES:
             typ = pb.type_(typings[0])
             if against in [t.name for t in typ.damage_relations.double_damage_to]:
                 eff_against1.append(against)
         eff_list = ", ".join(str(eff_type.capitalize()) for eff_type in eff_against1)
-        print("This Pokémon is effective against: " + eff_list + " types.")
+        print("This Pokémon is super effective against: " + eff_list + " types.")
 
 
 # get types pokémon is weak against
